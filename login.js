cb00b2f23c8a0c092c49f8b5d400edd52764021d
araxis07 <pavornprat.aa@gmail.com>
2023-12-01 23:17:04 +0700

    all change 1/12/2023

---
+++ login.js
@@ -0,0 +1,37 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+function auth() {
+    var email_check = !!document.getElementById('Emailcheck').value;
+    var password_check = !!document.getElementById('PasswordCheck').value;
+    var password_con_check = !!document.getElementById('Password_Confirm_Check').value;
+    var user_check = !!document.getElementById('Usercheck').value;
+
+    var password_syntax =  document.getElementById('PasswordCheck').value;
+    var password_con_syntax = document.getElementById('Password_Confirm_Check').value;
+
+    if (password_syntax !== password_con_syntax) {
+      // Perform your authentication logic
+      alert("Password not match Please try again.");
+      return;
+    }
+
+    else if (email_check && password_check && password_con_check && user_check) {
+      // Perform your authentication logic
+      window.location.href = "index2.html";
+      alert("Create Account Success.");
+
+
+    } else {
+      alert("Please fill in all fields.");
+    }
+    }
